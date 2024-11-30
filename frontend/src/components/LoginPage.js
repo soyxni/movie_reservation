066f500485f86f2747786f757afe928ddbd16c67
soyxni <weehee6947@gmail.com>
2024-12-01 05:13:46 +0900

    로그인 페이지 ui 수정

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -16,18 +16,15 @@ const LoginPage = ({ setIsAuthenticated }) => {
         },
         body: JSON.stringify({ email, password }),
       });
-  
+
       if (response.ok) {
         const data = await response.json();
-        console.log("JWT Access Token:", data.access); // 콘솔에 액세스 토큰 출력
-        localStorage.setItem("token", data.access); // 토큰 저장
-        setIsAuthenticated(true); // 로그인 상태 업데이트
-        console.log("LocalStorage after login:", localStorage.getItem("token")); // 저장된 값 출력
+        console.log("JWT Access Token:", data.access);
+        localStorage.setItem("token", data.access);
+        setIsAuthenticated(true);
         alert("로그인 성공");
-        navigate("/"); // 메인 페이지로 이동
+        navigate("/");
       } else {
-        const errorData = await response.json();
-        console.error("Login failed:", errorData);
         alert("로그인 실패. 이메일과 비밀번호를 확인하세요.");
       }
     } catch (error) {
@@ -37,29 +34,39 @@ const LoginPage = ({ setIsAuthenticated }) => {
 
   return (
     <div className="login-page">
-      <h1>로그인</h1>
-      <input
-        type="email"
-        placeholder="이메일"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="input-field"
-      />
-      <input
-        type="password"
-        placeholder="비밀번호"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="input-field"
-      />
-      <button onClick={handleLogin} className="login-button">
-        로그인
-      </button>
-      <p>
-        <span className="register-link" onClick={() => navigate("/register")}>
-          회원가입 페이지로 이동
+      {/* 상단바 */}
+      <header className="header">
+        <span onClick={() => navigate("/")} className="back-button">
+          ← 뒤로가기
         </span>
-      </p>
+        <h1 className="title">로그인</h1>
+      </header>
+
+      {/* 로그인 폼 */}
+      <div className="login-form">
+        <input
+          type="email"
+          placeholder="이메일"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="input-field"
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input-field"
+        />
+        <button onClick={handleLogin} className="login-button">
+          로그인
+        </button>
+        <p>
+          <span className="register-link" onClick={() => navigate("/register")}>
+            회원가입 페이지로 이동
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
