6b82e526525669fd7896c0c07b9117bc93a0c7ca
soyxni <weehee6947@gmail.com>
2024-12-01 02:26:03 +0900

    movie 시리얼라이저 수정 - 상영시간 추가

--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
 from .models import Movie, Screen
+from reservations.models import Showtime
 
+# Showtime 시리얼라이저
+class ShowtimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Showtime
+        fields = ['id', 'start_time', 'end_time', 'screen']
 
 # 영화 시리얼라이저
 class MovieSerializer(serializers.ModelSerializer):
+    showtimes = serializers.SerializerMethodField()  # Showtime 데이터를 커스텀 필드로 추가
+
     class Meta:
         model = Movie
-        fields = '__all__'  # 모든 필드 포함
+        fields = ['id', 'title', 'genre', 'duration', 'description', 'age_limit', 'showtimes']
 
+    def get_showtimes(self, obj):
+        # Showtime에서 해당 영화와 연결된 상영 시간 필터링
+        showtimes = Showtime.objects.filter(movie=obj)  # 특정 영화의 Showtime만 가져옴
+        return ShowtimeSerializer(showtimes, many=True).data
 
 # 상영관 시리얼라이저
 class ScreenSerializer(serializers.ModelSerializer):
