5da5e422d4ad3430e3144f1d871c5588ae09dfc5
soyxni <weehee6947@gmail.com>
2024-11-30 17:06:14 +0900

    reservation 앱 model.py 수정

--- backend/reservations/models.py
+++ backend/reservations/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Reservation(models.Model):
+    user = models.ForeignKey('User', models.DO_NOTHING)
+    showtime = models.ForeignKey('Showtime', models.DO_NOTHING)
+    seat = models.ForeignKey('Seat', models.DO_NOTHING)
+    reservation_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'reservation'
+
+
+class Seat(models.Model):
+    screen = models.ForeignKey('Screen', models.DO_NOTHING)
+    seat_number = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'seat'
+        unique_together = (('screen', 'seat_number'),)
+
+
+class Showtime(models.Model):
+    movie = models.ForeignKey('Movie', models.DO_NOTHING)
+    screen = models.ForeignKey('Screen', models.DO_NOTHING)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    class Meta:
+        db_table = 'showtime'
