d30f2d326b0aa5709379038903780a3602b82245
soyxni <weehee6947@gmail.com>
2024-12-01 16:20:38 +0900

    영화 조회 UI 및 CSS 수정

--- frontend/src/components/MoviePage.js
+++ frontend/src/components/MoviePage.js
@@ -1,41 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./MoviePage.css";
 
 const MoviePage = () => {
   const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
 
-  // 영화 데이터 가져오기
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/movies/movies/") // Django API URL
+    fetch("http://127.0.0.1:8000/api/movies/movies/")
       .then((response) => response.json())
-      .then((data) => {
-        console.log("Fetched data for MoviePage:", data);
-        setMovies(data); // 데이터를 상태로 설정
-      })
-      .catch((error) => console.error("Error fetching movies for MoviePage:", error));
+      .then((data) => setMovies(data))
+      .catch((error) => console.error("Error fetching movies:", error));
   }, []);
 
   return (
     <div className="movie-page">
-      {/* 상단 바 */}
+      {/* 상단바 */}
       <header className="header">
-        <Link to="/" className="back-button">← 뒤로가기</Link>
+        <span className="back-text" onClick={() => navigate("/")}>
+          ← back
+        </span>
         <h1 className="title">영화 조회</h1>
       </header>
 
-      {/* 영화 목록 */}
+      {/* 영화 리스트 */}
       <div className="movie-list">
         {movies.map((movie) => (
           <div key={movie.id} className="movie-card">
             <div className="movie-title">
               <h2>{movie.title}</h2>
-              <p>장르: {movie.genre}</p>
-              <p>러닝타임: {movie.duration}분</p>
-              <p>설명: {movie.description}</p>
-              <p>관람가: {movie.age_limit}세 이상</p>
+              <p>
+                <strong>장르:</strong> {movie.genre}
+              </p>
+              <p>
+                <strong>러닝타임:</strong> {movie.duration}분
+              </p>
+              <p>
+                <strong>설명:</strong> {movie.description}
+              </p>
+              <p>
+                <strong>관람가:</strong> {movie.age_limit}세 이상
+              </p>
             </div>
-            {/* <button className="reserve-button">예매하러 가기</button> */}
           </div>
         ))}
       </div>
