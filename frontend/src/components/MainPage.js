540a9bfec599082c08b6948fc9885d2733700894
soyxni <weehee6947@gmail.com>
2024-12-01 15:26:08 +0900

    MainPasge CSS 및 UI 수정

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -32,46 +32,52 @@ const MainPage = ({ isAuthenticated, setIsAuthenticated }) => {
   };
 
   return (
-    <div className="main-container">
-      {/* 로그인/로그아웃 링크 */}
-      <div className="login-link">
-        {isAuthenticated ? (
-          <span onClick={handleLogout} className="link-text">
-            로그아웃
-          </span>
-        ) : (
-          <Link to="/login" className="link-text">
-            로그인
-          </Link>
-        )}
+    <>
+      {/* 상단바 */}
+      <div className="navbar">
+        <div className="logo">CineTicket</div>
+        <div className="login-link">
+          {isAuthenticated ? (
+            <span onClick={handleLogout} className="link-text">
+              로그아웃
+            </span>
+          ) : (
+            <Link to="/login" className="link-text">
+              로그인
+            </Link>
+          )}
+        </div>
       </div>
 
-      {/* 메인 버튼 영역 */}
-      <div className="button-container">
-        <button className="main-button">
-          <Link to="/movies" className="button-link">
-            영화 조회
-          </Link>
-        </button>
-        <button className="main-button">
-          <Link to="/showtimes" className="button-link">
-            상영 시간 조회
-          </Link>
-        </button>
-        <button className="main-button">
-          <Link to="/reservations" className="button-link">
-            예매 내역 확인
-          </Link>
-        </button>
-      </div>
+      {/* 메인 컨테이너 */}
+      <div className="main-container">
+        {/* 메인 버튼 영역 */}
+        <div className="button-container">
+          <button className="main-button">
+            <Link to="/movies" className="button-link">
+              영화 조회
+            </Link>
+          </button>
+          <button className="main-button">
+            <Link to="/showtimes" className="button-link">
+              상영 시간 조회
+            </Link>
+          </button>
+          <button className="main-button">
+            <Link to="/reservations" className="button-link">
+              예매 내역 확인
+            </Link>
+          </button>
+        </div>
 
-      {/* 관리자 모드 링크 */}
-      <div className="admin-mode">
-        <button className="admin-button" onClick={handleAdminMode}>
-          관리자 모드
-        </button>
+        {/* 관리자 모드 링크 */}
+        <div className="admin-mode">
+          <button className="admin-button" onClick={handleAdminMode}>
+            관리자 모드
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
