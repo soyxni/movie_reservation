c837f9fd192880999bad6595859fa4d372373ebf
soyxni <weehee6947@gmail.com>
2024-12-01 12:56:17 +0900

    관리자 권한(영화, 상영관 관리) 백엔드 코드 추가

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import MovieViewSet, ScreenViewSet
+from .views import MovieViewSet, ScreenViewSet, admin_mode
+from django.urls import path
 
 router = DefaultRouter()
 router.register('movies', MovieViewSet, basename='movie')  # 영화 관리
 router.register('screens', ScreenViewSet, basename='screen')  # 상영관 관리
 
-urlpatterns = router.urls
+# DefaultRouter에서 생성한 URL과 추가로 정의한 URL을 결합
+urlpatterns = router.urls + [
+    path('admin-mode/', admin_mode, name='admin-mode'),  # 관리자 모드 엔드포인트 추가
+]
--- backend/movies/views.py
+++ backend/movies/views.py
@@ -6,6 +6,9 @@ from rest_framework.permissions import BasePermission, IsAuthenticated
 from .models import Movie, Screen
 from .serializers import MovieSerializer, ScreenSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
 
 
 
@@ -26,18 +29,22 @@ class IsManager(BasePermission):
         # 로그인된 사용자만 접근 가능 & staff_role이 'Manager'인 경우
         return request.user.is_authenticated and request.user.role == 'Staff' and request.user.staff_role == 'Manager'
 
+@api_view(['GET'])
+@permission_classes([IsManager])
+def admin_mode(request):
+    return Response({"message": "Welcome to admin mode!"})
 
 # 영화 관리 ViewSet
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    #permission_classes = [IsAuthenticated, IsManager]  # 인증된 사용자 + 관리자만 접근 가능
-    permission_classes = [AllowAny]
+    permission_classes = [IsManager]  # 인증된 사용자 + 관리자만 접근 가능
+    # permission_classes = [AllowAny]
 
 
 # 상영관 관리 ViewSet
 class ScreenViewSet(viewsets.ModelViewSet):
     queryset = Screen.objects.all()
     serializer_class = ScreenSerializer
-    #permission_classes = [IsAuthenticated, IsManager]  # 인증된 사용자 + 관리자만 접근 가능
-    permission_classes = [AllowAny]
+    permission_classes = [IsManager]  # 인증된 사용자 + 관리자만 접근 가능
+    # permission_classes = [AllowAny]
